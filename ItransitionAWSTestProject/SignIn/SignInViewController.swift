dc6baf454faf17b411180941d54d63c124e102e4
Sak, Andrey2 <a2.sak@itransition.com>
2017-04-29 10:57:32 +0300

    init controllers

---
+++ ItransitionAWSTestProject/SignIn/SignInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewController.swift
+//  ItransitionAWSTestProject
+//
+//  Created by Sak, Andrey2 on 4/29/17.
+//  Copyright © 2017 Sak, Andrey2. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
