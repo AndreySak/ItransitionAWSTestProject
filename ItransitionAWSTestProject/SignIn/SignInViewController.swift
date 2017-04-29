89bf979d2e6c9ef2b67f1fe257899e2cc8807e62
Zbranevich, Andrey <a.zbranevich@itransition.com>
2017-04-29 14:42:11 +0300

    implement  auth service

--- ItransitionAWSTestProject/SignIn/SignInViewController.swift
+++ ItransitionAWSTestProject/SignIn/SignInViewController.swift
@@ -7,11 +7,36 @@
 //
 
 import UIKit
+import AWSCognitoUserPoolsSignIn
 
 class SignInViewController: UIViewController {
 
+    var passwordAuthenticationCompletion: AWSTaskCompletionSource<AnyObject>?
+
     override func viewDidLoad() {
+        let credentialProvider = AWSCognitoCredentialsProvider(regionType: .USEast1, identityPoolId: "us-east-1_oAXrga09j")
+        let configuration = AWSServiceConfiguration(region: .USEast1, credentialsProvider: credentialProvider)
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+
+        AWSCognitoUserPoolsSignInProvider.sharedInstance().setInteractiveAuthDelegate(AuthService(username: "tester", password: "12345678"))
+
         super.viewDidLoad()
+    }
+
+    @IBAction func logInAction() {
+        if AWSSignInManager.sharedInstance().isLoggedIn {
+            AWSSignInManager.sharedInstance().logout(completionHandler: { _, _, _ in
+                print("LOGOUT")
+
+                AWSSignInManager.sharedInstance().login(signInProviderKey: AWSCognitoUserPoolsSignInProvider.sharedInstance().identityProviderName, completionHandler: {(result: Any?, authState: AWSIdentityManagerAuthState, error: Error?) in
+                    print("result = \(result), error = \(error)")
+                })
+            })
+        } else {
+            AWSSignInManager.sharedInstance().login(signInProviderKey: AWSCognitoUserPoolsSignInProvider.sharedInstance().identityProviderName, completionHandler: {(result: Any?, authState: AWSIdentityManagerAuthState, error: Error?) in
+                print("result = \(result), error = \(error)")
+            })
+        }
 
     }
 
